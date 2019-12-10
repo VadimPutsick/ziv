f90fab0d08522e43be8f73fbb8665434f31b5ff6
Vladislav Milto <vladislav.milto@artezio.com>
2019-12-10 18:06:51 +0300

    Excel export implementation

---
+++ ExcelExporter/Exporter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using CuttingTask;
+using Microsoft.Office.Interop.Excel;
+
+namespace ExcelExporter
+{
+    public class Exporter
+    {
+        private readonly string _path;
+        public Exporter(string path)
+        {
+            _path = path;
+            
+        }
+
+        public void WriteFile(IEnumerable<Plank> data)
+        {
+            var app = new Application
+            {
+                Visible = true,
+                DisplayAlerts = false,
+            };
+            var workbook = app.Workbooks.Add(Type.Missing);
+            var worksheet = (Worksheet)workbook.ActiveSheet;
+            worksheet.Name = "Results";
+            worksheet.Cells[2, 1] = "№";
+            worksheet.Cells[2, 2] = "Заказ";
+            worksheet.Cells[2, 3] = "Длина";
+            worksheet.Cells[2, 4] = "Гильза";
+            worksheet.Cells[2, 5] = "Было";
+            worksheet.Cells[2, 5] = "Сколько изготовить";
+            worksheet.Cells[2, 5] = "Отходы";
+            worksheet.Cells[2, 5] = "В процентах";
+            worksheet.Cells[2, 6] = "Сколько раз нужно изготовить";
+            worksheet.Cells[2, 7] = "Отходы";
+            var startFromRow = 3;
+            var index = 0;
+            foreach (var plank in data)
+            {
+                index++;
+                worksheet.Cells[startFromRow, 1] = index;
+                var i = 0;
+                for (i = 0; i < plank.Cuts.Count; i++)
+                {
+                    worksheet.Cells[startFromRow + i, 2].NumberFormat = "@";
+                    worksheet.Cells[startFromRow + i, 2] = plank.Names[i];
+                    worksheet.Cells[startFromRow + i, 3] = plank.Cuts[i];
+                    worksheet.Cells[startFromRow + i, 4] = plank.Sleeves[i];
+                    worksheet.Cells[startFromRow + i, 5] = plank.InitialCount[i];
+                }
+                worksheet.Range[worksheet.Cells[startFromRow, 1], worksheet.Cells[startFromRow + i - 1, 1]].Merge();
+                worksheet.Range[worksheet.Cells[startFromRow, 6], worksheet.Cells[startFromRow + i - 1, 6]].Merge();
+                worksheet.Range[worksheet.Cells[startFromRow, 7], worksheet.Cells[startFromRow + i - 1, 7]].Merge();
+
+                ((Style)worksheet.Range[worksheet.Cells[startFromRow, 6], worksheet.Cells[startFromRow, 7]].Style).VerticalAlignment = XlVAlign.xlVAlignCenter;
+                ((Style)worksheet.Range[worksheet.Cells[startFromRow, 6], worksheet.Cells[startFromRow, 7]].Style).HorizontalAlignment =
+                    XlHAlign.xlHAlignCenter;
+
+                //((Style)((Range)worksheet.Cells[startFromRow, 6]).Style).VerticalAlignment = XlVAlign.xlVAlignCenter;
+                //((Style) ((Range) worksheet.Cells[startFromRow, 6]).Style).HorizontalAlignment =
+                //    XlHAlign.xlHAlignCenter;
+                //((Style)((Range)worksheet.Cells[startFromRow, 7]).Style).VerticalAlignment = XlVAlign.xlVAlignCenter;
+                //((Style)((Range)worksheet.Cells[startFromRow, 7]).Style).HorizontalAlignment =
+                //    XlHAlign.xlHAlignCenter;
+                worksheet.Cells[startFromRow, 6] = plank.HowMany;
+                worksheet.Cells[startFromRow, 7] = plank.FreeLength;
+                
+
+
+
+                startFromRow += i;
+            }
+        }
+    }
+}
--- TestingConsole/Tester.cs
+++ TestingConsole/Tester.cs
@@ -4,82 +4,77 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using CuttingTask;
+using ExcelExporter;
 
 namespace TestingConsole
 {
-    class Tester
+    internal class Tester
     {
-        static void Main()
+        private static void Main()
         {
             Console.OutputEncoding = Encoding.UTF8;
+
             #region Считывание Ширины Количества Имён и проверка
-            List<int> desired = new List<int>(150);
-            using (StreamReader reader = new StreamReader("widthes.txt"))
+
+            var desired = new List<int>(150);
+            using (var reader = new StreamReader("widthes.txt"))
             {
                 string strNum;
-                while ((strNum = reader.ReadLine()) != null)
-                {
-                    desired.Add(Convert.ToInt32(strNum));
-                }
+                while ((strNum = reader.ReadLine()) != null) desired.Add(Convert.ToInt32(strNum));
             }
 
-            List<string> names = new List<string>(desired.Count);
-            using (StreamReader reader = new StreamReader("names.txt"))
+            var names = new List<string>(desired.Count);
+            using (var reader = new StreamReader("names.txt"))
             {
                 string name;
-                while ((name = reader.ReadLine()) != null)
-                {
-                    names.Add(name);
-                }
+                while ((name = reader.ReadLine()) != null) names.Add(name);
             }
 
-            List<decimal> counts = new List<decimal>(desired.Count);
-            using (StreamReader reader = new StreamReader("counts.txt"))
+            var counts = new List<decimal>(desired.Count);
+            using (var reader = new StreamReader("counts.txt"))
             {
                 string strNum;
-                while ((strNum = reader.ReadLine()) != null)
-                {
-                    counts.Add(Convert.ToDecimal(strNum));
-                }
+                while ((strNum = reader.ReadLine()) != null) counts.Add(Convert.ToDecimal(strNum));
             }
 
             if (counts.Count != desired.Count || desired.Count != names.Count)
             {
                 throw new Exception("Не равное количество параметров");
             }
+
             #endregion
 
             const int POSSIBLE_LENGTH = 6000;
             const int LIMIT_ON_CUTS = 6;
-            List<CommonInfo> commonInfoList = new List<CommonInfo>(desired.Count);  
-            for (var i = 0; i < desired.Count; i++)
-            {
-                commonInfoList.Add(new CommonInfo(names[i], desired[i], counts[i]));                
-            }
-            
+            var commonInfoList = desired.Select((t, i) => new CommonInfo(names[i], t, counts[i])).ToList();
+
             #region Агрегация материалов с одинаковым Width
+
             var elementsCount = commonInfoList.Select(x => x.Width).Distinct();
             var resultList = new List<CommonInfo>();
 
             foreach (var item in elementsCount)
             {
                 var tmp = commonInfoList.Where(x => x.Width == item);
-                resultList.Add(tmp.Aggregate((prev, next) => {
+                resultList.Add(tmp.Aggregate((prev, next) =>
+                {
                     prev.Name += ", " + next.Name;
                     prev.Count += next.Count;
                     return prev;
                 }));
             }
+
             #endregion
 
-            Solver solver = new Solver();
-            List<Plank> planks = solver.CalculateCuts(POSSIBLE_LENGTH, LIMIT_ON_CUTS, resultList);
-{
-                foreach (var plank in planks)
+            var solver = new Solver();
+            var planks = solver.CalculateCuts(POSSIBLE_LENGTH, LIMIT_ON_CUTS, resultList);
             {
-                Console.WriteLine(plank);
+                foreach (var plank in planks) Console.WriteLine(plank);
             }
-            }
+
+            var exporter = new Exporter(@"result.xlsx");
+
+            exporter.WriteFile(planks);
 
             Console.ReadKey(true);
         }
